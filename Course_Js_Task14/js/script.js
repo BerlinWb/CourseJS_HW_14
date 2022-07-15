559e711fc63daa0a29577f9424e6b0488f762a77
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-15 18:22:28 +0300

    Update script.js

--- Course_Js_Task14/js/script.js
+++ Course_Js_Task14/js/script.js
@@ -18,18 +18,20 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // 4
+
 const arr = [1, 2, 3, 4, 5];
-for (let i = 0; i < arr.length; i++) { 
-  console.log("Found " + arr[i]);
+let result = null;
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i];
 }
+console.log("sum:", result);
 
 // 5
 /*
 const arr = [1, 2, 3, 4, 5];
-let result;
+let result = null;
 for (let i = 0; i < arr.length; i++) {
-  result = Math.pow(arr[i], 2);
-  console.log(Math.pow(arr[i], 2));
+  result += Math.pow(arr[i], 2);
 }
 console.log("sum:", result);
 */
